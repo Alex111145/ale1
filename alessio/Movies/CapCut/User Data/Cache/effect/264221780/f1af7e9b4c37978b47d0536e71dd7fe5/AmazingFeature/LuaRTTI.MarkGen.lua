0f4ef9b9dc5f64f8059aa6c0e529e05fb9a7aace
Ale <159631777+Alex111145@users.noreply.github.com>
2025-02-25 17:33:13 +0100

    Delete alessio directory

--- alessio/Movies/CapCut/User Data/Cache/effect/264221780/f1af7e9b4c37978b47d0536e71dd7fe5/AmazingFeature/LuaRTTI.MarkGen.lua
+++
@@ -1,7 +0,0 @@
-
-SeekModeScript = SeekModeScript or {} ---@type SeekModeScript
-SeekModeScript.__typename = "SeekModeScript"
-SeekModeScript.__supername = "ScriptComponent"
-SeekModeScript.__scriptname = ""
-SeekModeScript.__scriptpath = ""
-
