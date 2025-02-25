88c3eec90fcc40a5e20be90263ed2f01ebef185a
stefanomomi <smomi@studenti.uninsubria.it>
2025-02-25 17:20:53 +0100

    provaaaa

---
+++ Documents/università/RMI/src/Client.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Client {
+    String name ;
+    Random rndRandom;
+
+    public Client() {
+        rndRandom = new Random();
+        this.name="Client" + rndRandom.nextInt(100);
+    }
+
+    public void exec()  throws IOException,NotBoundException{
+        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+        depositoInterface depositoInterface = (depositoInterface) registry.lookup("server");
+
+        long lastTime=System.currentTimeMillis()-1000000;
+        for(int j=0; j<10; j++) {
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextInt(300));
+            } catch (InterruptedException e) { }
+
+            Informazione lettura;
+            lettura = depositoInterface.read(lastTime);
+            System.out.println(name+" leggo "+lettura);
+            lastTime=lettura.getInfoTime();
+        }
+    }
+
+    public static void main(String[] args) throws NotBoundException{
+        try {
+            new Client().exec();
+        } catch (NotBoundException | IOException e) {
+            System.err.println("Client KO");
+        }
+    }
+}
