f7656352408380169495215160b4cdf9b59f30cf
Ale <159631777+Alex111145@users.noreply.github.com>
2025-02-25 14:43:45 +0100

    Delete 2 Anno directory

--- 2 Anno/Lab b (6cfu)/Lab A (3cfu) 3 settembre/cose utili per lab a/tutorial/Lab A Nico/versione 0.5/src/soluzione/StatoRegistrazione.java
+++
@@ -1,8 +0,0 @@
-package soluzione;
-/**
- * Vincolare la variabile StatoRegistrazione a poter assumere solo questi determinati valori.
- * 
- */
-public enum StatoRegistrazione {
-	IN, OUT
-}
--- 2 Anno/Lab b (6cfu)/Lab A (3cfu) 3 settembre/cose utili per lab a/tutorial/Lab A Nico/versione 0.5/src/soluzione/TipoComando.java
+++
@@ -1,8 +0,0 @@
-package soluzione;
-/**
- * Vincolare la variabile TipoComando a poter assumere solo questi determinati valori.
- * 
- */
-public enum TipoComando {
-	IMPORTA, CREA_MAPPA
-}
