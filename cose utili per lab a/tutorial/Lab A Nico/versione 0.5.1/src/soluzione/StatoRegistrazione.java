e8c97fe959a16ca9f702b8b46e8ddaf3168d2582
Alex111145 <alessio.gervasini45@gmail.com>
2025-02-25 11:57:22 +0100

    Backup cose utili per lab a/tutorial/Lab A Nico/versione 0.5.1/src/soluzione/ManagerEventi.java

---
+++ cose utili per lab a/tutorial/Lab A Nico/versione 0.5.1/src/soluzione/StatoRegistrazione.java
@@ -0,0 +1,7 @@
+package soluzione;
+/**
+ * Stato della registrazione degli utenti gestiti dal sistema.
+ */
+public enum StatoRegistrazione {
+	IN, OUT
+}
---
+++ cose utili per lab a/tutorial/Lab A Nico/versione 0.5.1/src/soluzione/TipoComando.java
@@ -0,0 +1,7 @@
+package soluzione;
+/**
+ * Tipologie di comandi gestiti dal sistema.
+ */
+public enum TipoComando {
+	IMPORTA, CREA_MAPPA
+}
