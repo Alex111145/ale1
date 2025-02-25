81915c04b468d65f7dea78d4aac4029590ebed31
Alex111145 <alessio.gervasini45@gmail.com>
2025-02-25 09:29:25 +0100

    Backup automatico dei file (tutti i tipi) mantenendo la struttura originale

---
+++ Sistemi operativi (8cfu)/sistemi operativi completo - ste/Esempi/Esempi Processi/Esempi uso fork/esempiofork_2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <unistd.h>
+
+int main(  ){
+    int p = getpid( ); 
+    printf("sono %d, ora mi  clono\n",p);
+    int x = fork( );     
+    p = getpid( );
+    
+    int y = 10;  
+    printf("y vale %d e io sono %d\n",y,p);  
+   
+    if(x==0){   
+        int f = getpid( );
+        printf("sono il figlio, cioè %d\n",f);
+        y=y+4;/* IL FIGLIO STAMPA 14  */ 
+        printf("y vale %d e io sono %d\n",y,p);
+    }
+   
+    else{        
+        printf("sono %d, mio figlio è %d\n",getpid( ),x);
+        y=y+700; /* IL PADRE STAMPA 710 */ 
+        printf("y vale %d e io sono %d\n",y,p);
+    }
+}
+
